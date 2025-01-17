72766da4239d5bef8751cc05942d425bdfe670fa
Ariq Aziz <muhaariq@gmail.com>
2025-01-17 14:31:28 +0700

    feat: middleware

---
+++ internal/middleware/middleware.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/aziz8860/forum-api/internal/configs"
+	"github.com/aziz8860/forum-api/pkg/jwt"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	secretKey := configs.Get().Service.SecretJWT
+
+	return func(c *gin.Context) {
+		header := c.Request.Header.Get("Authorization")
+
+		header = strings.TrimSpace(header)
+		if header == "" {
+			c.AbortWithError(http.StatusUnauthorized, errors.New("missing token"))
+			return
+		}
+
+		userID, username, err := jwt.ValidateToken(header, secretKey)
+		if err != nil {
+			c.AbortWithError(http.StatusUnauthorized, err)
+			return
+		}
+
+		c.Set("userID", userID)
+		c.Set("username", username)
+		c.Next() // supaya kalau nanti ada middlleware lainnya, dia akan meneruskan ke middleware selanjutnya
+	}
+}
